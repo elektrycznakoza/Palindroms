5351f6db455b47dc0108d088e2415e970766d810
Leszek Stanisławski <lst882n@gmail.com>
2023-12-18 11:43:29 +0100

    PEP8

--- Palindroms.py
+++ Palindroms.py
@@ -1,18 +1,18 @@
 def finding_symmetry(word):
 # Usunięcie białych znaków i zmiana na małe litery
-  word = word.replace(" ", "").lower()
-  return word == word[::-1]
+    word = word.replace(" ", "").lower()
+    return word == word[::-1]
 
-# Lista wyrazów do sprawdzenia
+# Lista do sprawdzenia
 words = ["kajak", "potop", "hello", "radar", "python", "level"]
 sentences = ["No lemon, no melon", "Stressed desserts", "Nobody's perfect", "Never odd or even", "Some men interpret nine memos", "Sad but true"]
 
-# Sprawdzenie każdego wyrazu w liście
+# Sprawdzenie każdego wyrazu
 for word in words:
-  result = finding_symmetry(word)
-  print(f'Czy "{word}" to palindrom? {result}')
+    result = finding_symmetry(word)
+    print(f'Czy "{word}" to palindrom? {result}')
 
-# Sprawdzenie każdej zdania w liście
+# Sprawdzenie każdego zdania
 for sentence in sentences:
-  result = finding_symmetry(sentence)
-  print(f'Czy "{sentence}" to palindrom? {result}')
+    result = finding_symmetry(sentence)
+    print(f'Czy "{sentence}" to palindrom? {result}')
